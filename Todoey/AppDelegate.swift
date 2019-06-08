e64d8d779155dbeb6d220a5c0d68b84e9f7d0eb7
Mark Harrison <mejharrison@hotmail.com>
2019-06-07 20:48:12 -0700

    Persisting data using Realm

---
+++ Todoey/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Todoey
+//
+//  Created by Mark Harrison on 6/5/19.
+//  Copyright © 2019 Mark Harrison. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+//        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        do {
+            _ = try Realm()
+        } catch {
+            print("Error initialising new realm, \(error)")
+        }
+        
+        return true
+    }
+}
---
+++ Todoey/Data Models/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Todoey
+//
+//  Created by Mark Harrison on 6/6/19.
+//  Copyright © 2019 Mark Harrison. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name: String = ""
+    let items = List<Item>()
+}
